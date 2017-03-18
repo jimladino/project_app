a5ca54bb61d9240563b6c5c3f48d69bceefc834a
Jim Ladino <jlstar1.jl@gmail.com>
2017-03-18 01:42:03 +0000

    Project Association Assignment Form Added!

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@ Programmer.create!([
   {name: "Joseph Ladino"},
   {name: "Jim Ladino"}
 ])
+Project.create!([
+  {programmer_id: 4, client_id: 1, project_name: "Grocery App"},
+  {programmer_id: 4, client_id: 4, project_name: "Projects App"},
+  {programmer_id: 3, client_id: 3, project_name: "Nailed-It"}
+])
